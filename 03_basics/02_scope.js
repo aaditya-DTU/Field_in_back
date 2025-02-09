f124d0ad3a362e293c511e2b80da8e0ddc1bb083
kirmada <aaditya_23mc001@dtu.ac.in>
2025-02-09 10:22:24 +0530

    understanding about scopes

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,11 +1,29 @@
+// var has global scope
+// scope is where we can access variable or not 
 
+// scope in windows is different from scope in browser
+let a = 300
 
 if (true) {
     let a = 10
     const b = 20
-    var c = 30
+    console.log("INNER: ", a);
 }
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+
+function one() {
+    const username = "Aadiii"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);    error cuz website is not accessible here
+    
+    two()
+}
+
+one()
+
