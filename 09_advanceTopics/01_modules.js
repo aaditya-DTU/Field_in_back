39f4f7cc057ce45c1aa8940bd015f7159d0bb454
kirmada <aaditya_23mc001@dtu.ac.in>
2025-03-10 10:27:38 +0530

    basic about api's

---
+++ 09_advanceTopics/01_modules.js
@@ -0,0 +1,4 @@
+// API is a talking language b/w any two systems or computers
+// eg : api.github.com/users/aaditya-dtu.....randomuser.me......
+
+// to study complex api requests....we use JSON formatter
